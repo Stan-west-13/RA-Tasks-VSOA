55864535238ae0b87992230a8cd1ae88d06c80a4
Swest13 <swest19@lsu.edu>
2026-06-09 12:30:01 -0500

    Emmeans

--- 002-Analysis.R
+++ 002-Analysis.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(lme4)
 library(lmerTest)
 library(emmeans)
+library(rstatix)
 library(ggplot2)
+library(modelsummary)
 source("R/Load_Helpers.R")
 
 ## Load most recent data
@@ -23,24 +25,68 @@ m <- lmer(vsoa ~ group+
           data = d %>% filter(!group == "ASD-NA"))
 summary(m)
 
+modelsummary(
+  list("Model" = m),
+  output = "data/modelsummary.docx",
+  stars = T,
+  title = "LMEM Results",
+  statistic = c("std.error","statistic","p.value"),
+  shape = term~model+statistic,
+)
+
+## Concreteness simple
+summary(lm(vsoa~concreteness_all_centered, data = d, subset = group == "ASD"))
+summary(lm(vsoa~concreteness_all_centered, data = d, subset = group == "NA"))
+
+## Iconicity simple
+summary(lm(vsoa~average_iconicity_written_centered, data = d, subset = group == "ASD"))
+summary(lm(vsoa~average_iconicity_written_centered, data = d, subset = group == "NA"))
+
 
 d_plot <- d %>%
   pivot_longer(cols = ends_with("centered"),
                names_to = "measure",
                values_to = "value")
-em <- emmeans(m,specs = c("group",
-                 "imageability_rating_centered",
-                 "concreteness_all_centered",
-                 "average_iconicity_written_centered"))
 
 ggplot(d_plot %>% filter(!group == "ASD-NA",
                          !measure %in% c("concreteness_perry_centered",
                                          "concreteness_brysbaert_centered")), aes(x = value, y = vsoa, color = group))+
-  geom_point()+
+  geom_point(alpha = 0.4)+
   geom_smooth(method = "lm")+
   facet_wrap(~measure,
-             scales = "free",
+             scales = "free",nrow = 3,ncol = 1,
              labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
                                       "concreteness_all_centered" = "Concreteness",
                                       "average_iconicity_written_centered" = "Iconicity")))+
   theme_bw()
+ggsave("Figures/VSOA_wordfeat_byGroup.png",
+       dpi = 300)
+
+## LG transformed
+
+
+d_lg10 <- d %>%
+  mutate(across(.cols = c(concreteness_all,average_iconicity_written),
+                .names = "{.col}_lg10",
+                .fns = log10))
+
+m <- lmer(vsoa ~ group+
+            imageability_rating_centered+
+            concreteness_all_centered+
+            average_iconicity_written_centered+
+            group*imageability_rating_centered+
+            group*concreteness_all_centered+
+            group*average_iconicity_written_centered+
+            (1 | word) + (1 | group),
+          control = lmerControl(optimizer = "Nelder_Mead"),
+          data = d %>% filter(!group == "ASD-NA"))
+summary(m)
+
+modelsummary(
+  list("Model" = m),
+  output = "data/modelsummary.docx",
+  stars = T,
+  title = "LMEM Results",
+  statistic = c("std.error","statistic","p.value"),
+  shape = term~model+statistic,
+)
