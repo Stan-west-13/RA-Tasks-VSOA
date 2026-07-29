3648098d0fe5a86dba12bc099c78294cde89f89e
Swest13 <swest19@lsu.edu>
2026-07-29 16:15:32 -0400

    Only using Winter et al. iconicity norms and adding CHILDES frequency to models

--- 001-DataSetup.R
+++ 001-DataSetup.R
@@ -7,7 +7,8 @@ d_VSOA <- read_rds("data/vsoa-autistic-nonautistic-ndar-id-fix-remodel-v2.rds")
 ezra_imag <- readxl::read_xlsx("data/Imageability_VSOA_SRCLD_may_analyses.xlsx")
 iconicity_conc <- read.csv("data/pone.0137147.s001.csv")
 concrete <- readxl::read_xlsx("data/13428_2013_403_MOESM1_ESM.xlsx")
-iconicity_new <- read.csv("data/iconicity_ratings_cleaned.csv")
+iconicity_new <- read.csv("data/iconicity_ratings_cleaned.csv") %>% 
+  mutate(word = tolower(word)) 
 ## Select relevant columms from Ezra table
 d_meta <- ezra_imag %>%
   select(num_item_id = `num_item_id...1`,
@@ -20,7 +21,8 @@ d_meta <- ezra_imag %>%
          group = Group,
          adjusted.VSOA,
          imageability_rating,
-         )
+         ) %>%
+  mutate(lemma = sub("\\+.*", "", lemma))
 
 ## Concreteness table
 conc_meta <- iconicity_conc %>%
@@ -29,36 +31,24 @@ conc_meta <- iconicity_conc %>%
   full_join(select(concrete, word = Word, concreteness_brysbaert = Conc.M))
 
 ## Iconicity table
-icon_meta <- iconicity_conc %>%
-  filter(task == "written") %>%
-  group_by(word) %>%
-  summarize(average_iconicity = mean(rating)) %>%
-  select(word, average_iconicity) %>%
-  rbind(select(iconicity_new, word, average_iconicity = rating)) %>%
-  mutate(word = tolower(word)) %>%
+icon_meta <- iconicity_new %>%
+  select(word, iconicity_rating = rating) %>%
   arrange(word) %>%
-  unique() %>%
-  filter(!duplicated(word))
+  unique() 
 
 ## Join with JCPP VSOA data
+## Only use winter.
 d_joined <- d_VSOA %>%
-  left_join(select(d_meta, num_item_id,lexical_class,aoa_produces, CHILDES_Freq, imageability_rating)) %>%
+  left_join(select(d_meta, lemma,num_item_id,lexical_class,aoa_produces, CHILDES_Freq, imageability_rating)) %>%
   unique() %>%
-  left_join(icon_meta) %>%
-  left_join(conc_meta) %>%
+  left_join(conc_meta, by = c("lemma" = "word")) %>%
+  left_join(icon_meta, by = c("lemma" = 'word')) %>%
   mutate(concreteness_all = ifelse(!is.na(concreteness_perry) & !is.na(concreteness_brysbaert), concreteness_brysbaert,
                                    ifelse(is.na(concreteness_perry),concreteness_brysbaert,
                                           ifelse(is.na(concreteness_brysbaert),concreteness_perry,NA)))) %>%
-  mutate(across(.cols = c(imageability_rating, average_iconicity, concreteness_all, concreteness_perry, concreteness_brysbaert),
+  mutate(across(.cols = c(imageability_rating, iconicity_rating, concreteness_all, concreteness_perry, concreteness_brysbaert),
                 .fns = ~.x - mean(.x, na.rm = T),
                 .names = "{.col}_centered")) %>%
   mutate(vsoa = ifelse(vsoa < 0, 0 , vsoa))
 
-
 saveRDS(d_joined, file = paste0("data/VSOA_Conc_Icon_Image_",Sys.Date(),".rds"))
-
-
-
-
-
-
--- 002-Analysis.R
+++ 002-Analysis.R
@@ -8,20 +8,27 @@ library(modelsummary)
 source("R/Load_Helpers.R")
 
 ## Load most recent data
-d <- load_most_recent_by_mtime("data","VSOA_Conc")
+d <- load_most_recent_by_mtime("data","VSOA_Conc") %>%
+  mutate(vsoa_adjusted = ifelse(vsoa > 680, 680, vsoa), .after = vsoa) %>%
+  filter(!group == "ASD-NA") %>%
+  mutate(group = droplevels(group),
+         group = as.factor(group))
 
-
+contrasts(d$group) <- c(-0.5,0.5)
 ## Linear mixed model VSOA ~ group + imageability + concreteness + iconicity + group*iconicity + group*concreteness + group*imageability
 
-m <- lmer(vsoa ~ group+
+d %>%
+  group_by(group) %>%
+  summarize(mean_VSOA = mean(vsoa_adjusted),
+            sd_VSOA = sd(vsoa_adjusted)) %>%
+  as.data.frame()
+
+m <- lmer(vsoa_adjusted ~ group+CHILDES_Freq+
                  imageability_rating_centered+
-                 concreteness_all_centered+
-                 average_iconicity_written_centered+
+                 iconicity_rating_centered+
                  group*imageability_rating_centered+
-                 group*concreteness_all_centered+
-                 group*average_iconicity_written_centered+
-            (1 | word) + (1 | group),
-          control = lmerControl(optimizer = "Nelder_Mead"),
+                 group*iconicity_rating_centered+
+            (1 | word),
           data = d %>% filter(!group == "ASD-NA"))
 summary(m)
 
@@ -39,9 +46,12 @@ summary(lm(vsoa~concreteness_all_centered, data = d, subset = group == "ASD"))
 summary(lm(vsoa~concreteness_all_centered, data = d, subset = group == "NA"))
 
 ## Iconicity simple
-summary(lm(vsoa~average_iconicity_written_centered, data = d, subset = group == "ASD"))
-summary(lm(vsoa~average_iconicity_written_centered, data = d, subset = group == "NA"))
+summary(lm(vsoa~iconicity_rating_centered+CHILDES_Freq, data = d, subset = group == "ASD"))
+summary(lm(vsoa~iconicity_rating_centered+CHILDES_Freq, data = d, subset = group == "NA"))
 
+## Imageability simple
+summary(lm(vsoa~imageability_rating, data = d, subset = group == "ASD"))
+summary(lm(vsoa~imageability_rating, data = d, subset = group == "NA"))
 
 d_plot <- d %>%
   pivot_longer(cols = ends_with("centered"),
@@ -50,43 +60,246 @@ d_plot <- d %>%
 
 ggplot(d_plot %>% filter(!group == "ASD-NA",
                          !measure %in% c("concreteness_perry_centered",
-                                         "concreteness_brysbaert_centered")), aes(x = value, y = vsoa, color = group))+
-  geom_point(alpha = 0.4)+
+                                         "concreteness_brysbaert_centered",
+                                         "concreteness_all_centered")), aes(x = value, y = vsoa, color = group))+
+ # geom_point(alpha = 0.4)+
   geom_smooth(method = "lm")+
   facet_wrap(~measure,
-             scales = "free",nrow = 3,ncol = 1,
+             scales = "free",nrow = 2,ncol = 1,
              labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
-                                      "concreteness_all_centered" = "Concreteness",
-                                      "average_iconicity_written_centered" = "Iconicity")))+
+                                      "iconicity_rating_centered" = "Iconicity")))+
   theme_bw()
 ggsave("Figures/VSOA_wordfeat_byGroup.png",
        dpi = 300)
 
-## LG transformed
 
 
-d_lg10 <- d %>%
-  mutate(across(.cols = c(concreteness_all,average_iconicity_written),
-                .names = "{.col}_lg10",
-                .fns = log10))
 
-m <- lmer(vsoa ~ group+
+## Add how much data we have for each word in the IVs
+
+d_numbs <- d %>% 
+  select(word,lexical_class, iconicity_rating,imageability_rating) %>%
+  pivot_longer(cols = c(iconicity_rating,imageability_rating),
+               names_to = "measure",
+               values_to = "value") %>%
+  unique() 
+
+## Overall
+d_numbs %>%
+  group_by(measure) %>%
+  summarize(n_NA = sum(is.na(value)),
+            n_nonNA = sum(!is.na(value)),
+            prop_NA = sum(is.na(value)/n()),
+            n = n())
+
+## By class
+d_numbs %>%
+  group_by(measure, lexical_class) %>%
+  summarize(n_NA = sum(is.na(value)),
+            n_nonNA = sum(!is.na(value)),
+            prop_NA = sum(is.na(value)/n()),
+            n = n())
+
+
+wrdlvl_nums <- d_numbs %>%
+  group_by(word) %>%
+  mutate(is.complete = ifelse(sum(!is.na(value)) == 2, TRUE,FALSE)) %>%
+  ungroup()
+
+wrdlvl_nums %>%
+  select(word,is.complete) %>%
+  unique() %>%
+  summarize(sum(is.complete))
+
+
+
+## Correlation between concreteness and imageability, iconcicity
+cor(d %>% 
+       select(word, concreteness_all, iconicity_rating,imageability_rating) %>%
+      unique %>%
+  select(-word), use = "pairwise.complete.obs", method = 'pearson') 
+
+
+
+## Nested models for imageability, imageability and concreteness. Or
+## Remove one and see if the model converges. 
+
+m <- lmer(vsoa_adjusted ~ group+
             imageability_rating_centered+
-            concreteness_all_centered+
-            average_iconicity_written_centered+
+            iconicity_rating+
             group*imageability_rating_centered+
-            group*concreteness_all_centered+
-            group*average_iconicity_written_centered+
+            group*iconicity_rating+
             (1 | word) + (1 | group),
           control = lmerControl(optimizer = "Nelder_Mead"),
           data = d %>% filter(!group == "ASD-NA"))
 summary(m)
 
+
+## Nouns #####
+m_nouns <- lmer(vsoa_adjusted ~ group+CHILDES_Freq+
+            imageability_rating_centered+
+            iconicity_rating_centered+
+            group*imageability_rating_centered+
+            group*iconicity_rating_centered+
+            (1 | word),
+          data = d %>% filter(!group == "ASD-NA", lexical_class == "nouns"))
+summary(m_nouns)
+
 modelsummary(
-  list("Model" = m),
-  output = "data/modelsummary.docx",
+  list("Model" = m_nouns),
+  output = "data/modelsummary_nouns.docx",
   stars = T,
-  title = "LMEM Results",
+  title = "LMEM Results Nouns",
   statistic = c("std.error","statistic","p.value"),
-  shape = term~model+statistic,
+  shape = term~model+statistic
 )
+
+ggplot(d_plot %>% filter(!group == "ASD-NA",
+                         lexical_class == "nouns",
+                         !measure %in% c("concreteness_perry_centered",
+                                         "concreteness_brysbaert_centered",
+                                         "concreteness_all_centered")), aes(x = value, y = vsoa, color = group))+
+  #geom_point(alpha = 0.4)+
+  geom_smooth(method = "lm")+
+  facet_wrap(~measure,
+             scales = "free",nrow = 3,ncol = 1,
+             labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
+                                      "iconicity_rating_centered" = "Iconicity")))+
+  theme_bw()
+ggsave("Figures/VSOA_wordfeat_byGroupNouns.png",
+       dpi = 300)
+
+
+
+## Verbs #####
+m_verbs <- lmer(vsoa_adjusted ~ group+CHILDES_Freq+
+                  imageability_rating_centered+
+                  iconicity_rating_centered+
+                  group*imageability_rating_centered+
+                  group*iconicity_rating_centered+
+                  (1 | word),
+                data = d %>% filter(!group == "ASD-NA", lexical_class == "verbs"))
+summary(m_verbs)
+
+modelsummary(
+  list("Model" = m_verbs),
+  output = "data/modelsummary_verbs.docx",
+  stars = T,
+  title = "LMEM Results Verbs",
+  statistic = c("std.error","statistic","p.value"),
+  shape = term~model+statistic
+)
+
+ggplot(d_plot %>% filter(!group == "ASD-NA",
+                         lexical_class == "verbs",
+                         !measure %in% c("concreteness_perry_centered",
+                                         "concreteness_brysbaert_centered",
+                                         "concreteness_all_centered")), aes(x = value, y = vsoa, color = group))+
+  #geom_point(alpha = 0.4)+
+  geom_smooth(method = "lm")+
+  facet_wrap(~measure,
+             scales = "free",nrow = 3,ncol = 1,
+             labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
+                                      "iconicity_rating_centered" = "Iconicity")))+
+  theme_bw()
+
+## Adjectives ######
+m_adj <- lmer(vsoa_adjusted ~ group+CHILDES_Freq+
+                  imageability_rating_centered+
+                  iconicity_rating_centered+
+                  group*imageability_rating_centered+
+                  group*iconicity_rating_centered+
+                  (1 | word),
+                data = d %>% filter(!group == "ASD-NA", lexical_class == "adjectives"))
+summary(m_adj)
+
+
+modelsummary(
+  list("Model" = m_adj),
+  output = "data/modelsummary_adj.docx",
+  stars = T,
+  title = "LMEM Results Adjective",
+  statistic = c("std.error","statistic","p.value"),
+  shape = term~model+statistic
+)
+
+ggplot(d_plot %>% filter(!group == "ASD-NA",
+                         lexical_class == "adjectives",
+                         !measure %in% c("concreteness_perry_centered",
+                                         "concreteness_brysbaert_centered",
+                                         "concreteness_all_centered")), aes(x = value, y = vsoa, color = group))+
+  #geom_point(alpha = 0.4)+
+  geom_smooth(method = "lm")+
+  facet_wrap(~measure,
+             scales = "free",nrow = 3,ncol = 1,
+             labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
+                                      "iconicity_rating_centered" = "Iconicity")))+
+  theme_bw()
+
+### Interaction
+m_image_adj_NA <- lm(vsoa_adjusted ~ imageability_rating_centered+CHILDES_Freq, 
+                     data = d,
+                     subset = c(group == "NA", lexical_class == "adjectives")) 
+summary(m_image_adj_NA)
+
+m_image_adj_ASD <- lm(vsoa_adjusted ~ imageability_rating_centered+CHILDES_Freq, 
+                     data = d,
+                     subset = c(group == "ASD", lexical_class == "adjectives")) 
+summary(m_image_adj_ASD)
+
+m_icon_adj_NA <- lm(vsoa_adjusted ~ iconicity_rating_centered+CHILDES_Freq, 
+                     data = d,
+                     subset = c(group == "NA", lexical_class == "adjectives")) 
+summary(m_icon_adj_NA)
+
+m_icon_adj_ASD <- lm(vsoa_adjusted ~ iconicity_rating_centered+CHILDES_Freq, 
+                      data = d,
+                      subset = c(group == "ASD", lexical_class == "adjectives")) 
+summary(m_icon_adj_ASD)
+
+
+## Other #####
+m_other <- lmer(vsoa_adjusted ~ group+CHILDES_Freq+
+                imageability_rating_centered+
+                iconicity_rating_centered+
+                group*imageability_rating_centered+
+                group*iconicity_rating_centered+
+                (1 | word),
+              data = d %>% filter(!group == "ASD-NA", lexical_class == "other"))
+summary(m_other)
+
+
+modelsummary(
+  list("Model" = m_other),
+  output = "data/modelsummary_other.docx",
+  stars = T,
+  title = "LMEM Results Other",
+  statistic = c("std.error","statistic","p.value"),
+  shape = term~model+statistic
+)
+
+
+ggplot(d_plot %>% filter(!group == "ASD-NA",
+                         lexical_class == "other",
+                         !measure %in% c("concreteness_perry_centered",
+                                         "concreteness_brysbaert_centered",
+                                         "concreteness_all_centered")), aes(x = value, y = vsoa, color = group))+
+  #geom_point(alpha = 0.4)+
+  geom_smooth(method = "lm")+
+  facet_wrap(~measure,
+             scales = "free",nrow = 3,ncol = 1,
+             labeller = as_labeller(c("imageability_rating_centered" = "Imageability",
+                                      "iconicity_rating_centered" = "Iconicity")))+
+  theme_bw()
+
+
+### Interaction
+m_icon_other_NA <- lm(vsoa_adjusted ~ iconicity_rating_centered+CHILDES_Freq, 
+                     data = d %>% filter(lexical_class == "other"),
+                     subset = c(group == "NA")) 
+summary(m_icon_other_NA)
+
+m_icon_other_ASD <- lm(vsoa_adjusted ~ iconicity_rating_centered+CHILDES_Freq, 
+                      data = d %>% filter(lexical_class == "other"),
+                      subset = c(group == "ASD")) 
+summary(m_icon_other_ASD)
